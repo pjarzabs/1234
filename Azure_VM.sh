45abb11965c7f187b3e7415e9c278ed2412a09d1
pjarzabs <01181664@pw.edu.pl>
2026-05-08 11:24:53 +0200

    Update Azure_VM.sh for new subscription and VM settings

--- Azure_VM.sh
+++ Azure_VM.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/zsh
 
 az login --use-device-code
-az account set -s 7376751c-7b85-4428-8ba3-23f179599e5f
 
-az group create -n LabSK -l swedencentral
+az account set -s TWOJ_SUBSCRIPTION_ID
+
+az group create -n LabSK -l polandcentral
 
 az vm create \
-  -g LabSK \
-  -n jarzabski-u24-vm \
-  --image Ubuntu2404 \
-  --admin-username $USER \
-  --ssh-key-value ~/.ssh/id_ed25519.pub
+    -n piotruspan123 \
+    -g LabSK \
+    --image Ubuntu2404 \
+    --size Standard_B1s \
+    --admin-username piotroot \
+    --ssh-key-values ~/.ssh/id_ed25519.pub
+
+
+az account list-locations -o table
+
+az vm list-sizes -l polandcentral -o table
